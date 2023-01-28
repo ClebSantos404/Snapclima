12ccb39b63d6d5e67117f9505f9ef99ca061dc71
Clebson Santos <clebsondantas404@gmail.com>
2023-01-28 09:05:43 -0300

    Usando api para buscar localizações

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Interação
-const citySearchBar = document.getElementById('city-search-input')
+const citySearchInput = document.getElementById('city-search-input')
 const citySearchButton = document.getElementById('city-search-button')
 
 // Exibição
@@ -13,3 +13,60 @@ const feelsLikeTemperature = document.getElementById("feels-like-temperature");
 const currentHumidity = document.getElementById("current-humidity");
 const sunriseTime = document.getElementById("sunrise-time");
 const sunsetTime = document.getElementById("sunset-time");
+
+const api_key = "65937e2ae31c78d2f47b15d00058b578";
+
+citySearchButton.addEventListener("click", () => {
+    let cityName = citySearchInput.value
+    getCityWeather(cityName)
+})
+
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    let lat = position.coords.latitude
+    let lon = position.coords.longitude
+  
+    console.log(position)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => displayWeather(data))
+  },
+  (err) => {
+    if (err.code === 1) {
+        alert("Geolocalização negada pelo usuário, busque manualmente por uma cidade através da barra de pesquisa.")
+    } else {
+      console.log(err)
+    }
+  }
+)
+
+function getCityWeather(cityName) {
+
+    weatherIcon.src = `/assets/loading-icon.svg`
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => displayWeather(data))
+}
+
+function displayWeather(data) {
+    let { 
+        dt,
+        name,
+        weather: [{ icon, description}],
+        main: { temp, feels_like, humidity },
+        wind: { speed },
+        sys: { sunrise, sunset },
+     } = data
+
+     currentDate.textContent = dt;
+     cityName.textContent = name;
+     weatherIcon.src = `/assets/${icon}.svg`
+     weatherDescription.textContent = description;
+     currentTemperature.textContent = temp;
+     windSpeed.textContent = speed;
+     feelsLikeTemperature.textContent = feels_like;
+     currentHumidity.textContent = humidity;
+     sunriseTime.textContent = sunrise;
+     sunsetTime.textContent = sunset;
+}
