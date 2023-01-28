3ff537c55b19b7deb8c3efb1aa429357d7b7b78f
Clebson Santos <clebsondantas404@gmail.com>
2023-01-28 11:52:35 -0300

    formatação dos dados finais

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Interação
-const citySearchInput = document.getElementById('city-search-input')
-const citySearchButton = document.getElementById('city-search-button')
+const citySearchInput = document.getElementById("city-search-input");
+const citySearchButton = document.getElementById("city-search-button");
 
 // Exibição
 const currentDate = document.getElementById("current-date");
@@ -17,69 +17,78 @@ const sunsetTime = document.getElementById("sunset-time");
 const api_key = "65937e2ae31c78d2f47b15d00058b578";
 
 citySearchButton.addEventListener("click", () => {
-    let cityName = citySearchInput.value
-    getCityWeather(cityName)
-})
+  let cityName = citySearchInput.value;
+  getCityWeather(cityName);
+});
 
 navigator.geolocation.getCurrentPosition(
   (position) => {
-    let lat = position.coords.latitude
-    let lon = position.coords.longitude
-  
-    console.log(position)
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${api_key}`)
-    .then((response) => response.json())
-    .then((data) => displayWeather(data))
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+
+    getCurrentLocationWeather(lat, lon)
   },
   (err) => {
     if (err.code === 1) {
-        alert("Geolocalização negada pelo usuário, busque manualmente por uma cidade através da barra de pesquisa.")
+      alert(
+        "Geolocalização negada pelo usuário, busque manualmente por uma cidade através da barra de pesquisa."
+      );
     } else {
-      console.log(err)
+      console.log(err);
     }
   }
 )
 
+function getCurrentLocationWeather(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${api_key}`)
+  .then((response) => response.json())
+  .then((data) => displayWeather(data))
+}
+
 function getCityWeather(cityName) {
+  weatherIcon.src = `/assets/loading-icon.svg`;
 
-    weatherIcon.src = `/assets/loading-icon.svg`
-
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`)
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=pt_br&appid=${api_key}`
+  )
     .then((response) => response.json())
-    .then((data) => displayWeather(data))
+    .then((data) => displayWeather(data));
 }
 
 function displayWeather(data) {
-    let { 
-        dt,
-        name,
-        weather: [{ icon, description}],
-        main: { temp, feels_like, humidity },
-        wind: { speed },
-        sys: { sunrise, sunset },
-     } = data
+  let {
+    dt,
+    name,
+    weather: [{ icon, description }],
+    main: { temp, feels_like, humidity },
+    wind: { speed },
+    sys: { sunrise, sunset },
+  } = data;
 
-     currentDate.textContent = formatDate(dt);
-     cityName.textContent = name;
-     weatherIcon.src = `/assets/${icon}.svg`
-     weatherDescription.textContent = description;
-     currentTemperature.textContent = temp;
-     windSpeed.textContent = speed;
-     feelsLikeTemperature.textContent = feels_like;
-     currentHumidity.textContent = humidity;
-     sunriseTime.textContent = formatTime(sunrise);
-     sunsetTime.textContent = formatTime(sunset);
+  currentDate.textContent = formatDate(dt);
+  cityName.textContent = name;
+  weatherIcon.src = `/assets/${icon}.svg`;
+  weatherDescription.textContent = description;
+  currentTemperature.textContent = `${Math.round(temp)}°C`;
+  windSpeed.textContent = `${Math.round(speed * 3.6)}km`;
+  feelsLikeTemperature.textContent = `${Math.round(feels_like)}°C`;
+  currentHumidity.textContent = `${humidity}%`;
+  sunriseTime.textContent = formatTime(sunrise);
+  sunsetTime.textContent = formatTime(sunset);
 }
 
 function formatDate(epochTime) {
-    let date = new Date(epochTime * 1000)
-    let formattedDate = date.toLocaleDateString('pt-BR', { month: "long", day: "numeric"})
-    return `Hoje, ${formattedDate}`
+  let date = new Date(epochTime * 1000)
+  let formattedDate = date.toLocaleDateString("pt-BR", {
+    month: "long",
+    day: "numeric",
+  });
+  return `Hoje, ${formattedDate}`
 }
 
 function formatTime(epochTime) {
-    let date = new Date(epochTime * 1000)
-    let hours = date.getHours()
-    let minutes = date.getMinutes()
-    return `${hours}:${minutes}`
+  let date = new Date(epochTime * 1000)
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  return `${hours}:${minutes}`
 }
