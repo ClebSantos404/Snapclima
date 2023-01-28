4d974d707a94722bec8c94620820e1dea16e328e
Clebson Santos <clebsondantas404@gmail.com>
2023-01-28 14:01:13 -0300

    ajustando quebra de imagem

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ function displayWeather(data) {
 
   currentDate.textContent = formatDate(dt);
   cityName.textContent = name;
-  weatherIcon.src = `/assets/${icon}.svg`;
+  weatherIcon.src = `assets/${icon}.svg`;
   weatherDescription.textContent = description;
   currentTemperature.textContent = `${Math.round(temp)}°C`;
   windSpeed.textContent = `${Math.round(speed * 3.6)}km`;
